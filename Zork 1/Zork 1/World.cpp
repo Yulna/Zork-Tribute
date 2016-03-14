753e359ff0947e4fb5484280f2b08a3f68200c8d
JoanPareja <joanparejapinyol@gmail.com>
2016-03-14 23:37:26 +0100

    Added all rooms loagical data and some descritions

--- Zork 1/Zork 1/World.cpp
+++ Zork 1/Zork 1/World.cpp
@@ -20,18 +20,70 @@ void World::Create_world(){
 	enum Rooms_names{Park_Start=0, Crush_house, Kid_house, Shop, Gross_guy_house, Dark_street, Nrw_street, Amus_park, Toilet, Library};
 	
 	//Park data
-	strcpy_s(((room + Park_Start)->name), "PARK");
-	strcpy_s(((room + Park_Start)->description), "THE STARTING POINT");
+	strcpy_s(((room + Park_Start)->name), "Park");
+	strcpy_s(((room + Park_Start)->description), "A simple kids park, the perfect home for a homless person like you. \nYou see a library to the south, and regular house at the north.");
 	(room + Park_Start)->north_exit = (room + Kid_house);
-	(room + Park_Start)->down_exit = (room + Kid_house);
-	(room + Park_Start)->open_down = false;
-
+	(room + Park_Start)->south_exit = (room + Library);
 	
 	//Kid house data
-	strcpy_s(((room + Kid_house)->name), "KIDDO");
-	strcpy_s(((room + Kid_house)->description ),"MOVED TO NORTH");
+	strcpy_s(((room + Kid_house)->name), "Annoying kid house");
+	strcpy_s(((room + Kid_house)->description ),"The place where that kid that never lets you take a nap lives. \nYou see a shop at the north and you're beatiful home (the park) at the south.");
+	(room + Kid_house)->north_exit = (room + Shop);
 	(room + Kid_house)->south_exit = (room + Park_Start);
+	
+	//Shop
+	strcpy_s(((room + Shop)->name), "Regular Shop");
+	strcpy_s(((room + Shop)->description), "A simple, regular, normal shop even the salesman is plain. \nAt least you can still buy some groceries, or steal them. \nYou see a normal hause to the south and a house with a strange aura to the east.");
+	(room + Shop)->south_exit = (room + Kid_house);
+	(room + Shop)->east_exit = (room + Gross_guy_house);
 
+	//Cross-dressing guy house
+	strcpy_s(((room + Gross_guy_house)->name), "Gross cross-dressing guy house");
+	strcpy_s(((room + Gross_guy_house)->description), "");
+	(room + Gross_guy_house)->south_exit = (room + Dark_street);
+	(room + Gross_guy_house)->weast_exit = (room + Shop);
+	(room + Gross_guy_house)->east_exit = (room + Amus_park);
+
+	//Dark street
+	strcpy_s(((room + Dark_street)->name), "Dark street");
+	strcpy_s(((room + Dark_street)->description), "");
+	(room + Dark_street)->north_exit = (room + Gross_guy_house);
+	(room + Dark_street)->south_exit = (room + Nrw_street);
+	(room + Dark_street)->open_south = false;
+
+	//Narrow street
+	strcpy_s(((room + Nrw_street)->name), "Narrow street");
+	strcpy_s(((room + Nrw_street)->description), "");
+	(room + Nrw_street)->north_exit = (room + Dark_street);
+	(room + Nrw_street)->east_exit = (room + Crush_house);
+	(room + Nrw_street)->open_east = false;
+
+	//Crush house
+	strcpy_s(((room + Crush_house)->name), "Crush house");
+	strcpy_s(((room + Crush_house)->description), "");
+	(room + Crush_house)->north_exit = (room + Amus_park);
+	(room + Crush_house)->weast_exit = (room + Nrw_street);
+
+	//Amusement park
+	strcpy_s(((room + Amus_park)->name), "Amusment park");
+	strcpy_s(((room + Amus_park)->description), "");
+	(room + Amus_park)->south_exit = (room + Crush_house);
+	(room + Amus_park)->weast_exit = (room + Gross_guy_house);
+	(room + Amus_park)->east_exit = (room + Toilet);
+
+	//Toilets
+	strcpy_s(((room + Toilet)->name), "Public toilets");
+	strcpy_s(((room + Toilet)->description), "");
+	(room + Toilet)->weast_exit = (room + Amus_park);
+	(room + Toilet)->down_exit = (room + Library);
+	(room + Toilet)->open_down = false;
+
+	//Library
+	strcpy_s(((room + Library)->name), "Library");
+	strcpy_s(((room + Library)->description), "");
+	(room + Library)->north_exit = (room + Park_Start);
+	(room + Library)->down_exit = (room + Toilet);
+	(room + Library)->open_down = false;
 }
 
 //Movement functions
